bb6f0c08e453cab6dbf8d6eb4b33ac7323fdfea4
Harold Thetiot <hthetiot@gmail.com>
2024-06-23 02:16:06 +0200

    fix extractCarry on onObjectDeletionCustom for InvaderTransportUnit and TemporalGateBuilding

--- js/units/invader_transport.js
+++ js/units/invader_transport.js
@@ -26,8 +26,7 @@ function InvaderTransportUnit(pos_x, pos_y, player)
 	
 	this.onObjectDeletionCustom = function() 
 	{
-		if (this.haveInsideUnits())
-			this.extract();
+		this.extractCarry();
 	};
 }
 
