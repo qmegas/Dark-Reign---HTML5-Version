b8373899a6a40ed221142455dc8bc1cbc111bdae
Harold Thetiot <hthetiot@gmail.com>
2025-08-05 15:50:14 +0200

    improve touch support

--- js/shell/scroll.js
+++ js/shell/scroll.js
@@ -37,7 +37,7 @@ function ScrollWidget()
 	
 	this._setHandlers = function()
 	{
-		$(options.button_up).bind('mousedown pointerdown', function(){
+		$(options.button_up).bind('mousedown touchstart', function(){
 			current_pos -= 10;
 			if (current_pos <= 0)
 				current_pos = 0;
@@ -45,22 +45,53 @@ function ScrollWidget()
 			self._scroll();
 		});
 		
-		$(options.button_down).bind('mousedown pointerdown', function(){
+		$(options.button_down).bind('mousedown touchstart', function(){
 			current_pos += 10;
 			if (current_pos > overflow_size)
 				current_pos = overflow_size;
 			
 			self._scroll();
 		});
+
+		// desktop scroll
+		$(options.scroll_element).bind( 'mousewheel DOMMouseScroll', function ( e ) {
+		    e.preventDefault();
+		    var e0 = e.originalEvent,
+		    delta = e0.wheelDelta || -e0.detail;
+
+		    current_pos += (delta * -1)  / scroll_size * overflow_size;
+			self._scroll();
+		});
+
+		var lastY;
+		var currentY;
+
+		// reset touch position on touchstart
+		$(options.scroll_element).bind('touchstart', function (e){
+		    e.preventDefault();
+		    var currentY = e.originalEvent.touches[0].clientY;
+		    lastY = currentY;
+		});
+
+		// get movement and scroll the same way
+		$(options.scroll_element).bind('touchmove', function (e){
+		    e.preventDefault();
+		    var currentY = e.originalEvent.touches[0].clientY;
+		    delta = currentY - lastY;
+
+		    current_pos += (delta * -1);
+			self._scroll();
+		    lastY = currentY;
+		});
 		
 		$(options.button_slider)
-			.bind('mousedown pointerdown', function(){
+			.bind('mousedown touchstart', function(){
 				slider_hold = true;
 			})
-			.bind('mouseup pointerup', function(){
+			.bind('mouseup mouseleave touchend', function(){
 				slider_hold = false;
 			})
-			.bind('mousemove mousemove', function(event){
+			.bind('mousemove touchmove', function(event){
 				if (slider_hold)
 				{
 					
@@ -80,9 +111,9 @@ function ScrollWidget()
 	
 	this._unsetHandlers = function()
 	{
-		$(options.button_up).unbind('mousedown pointerdown');
-		$(options.button_down).unbind('mousedown  pointerdown');
-		$(options.button_slider).unbind('mousedown pointerdown mouseup pointerup mousemove pointermove').hide();
+		$(options.button_up).unbind('mousedown touchstart');
+		$(options.button_down).unbind('mousedown  touchstart');
+		$(options.button_slider).unbind('mousedown touchstart mouseup touchend touchmove').hide();
 		
 		is_set = false;
 		slider_hold = false;
@@ -90,6 +121,12 @@ function ScrollWidget()
 	
 	this._scroll = function()
 	{
+		if (current_pos > overflow_size)
+				current_pos = overflow_size;
+
+		if (current_pos < 0)
+			current_pos = 0
+
 		$(options.scroll_element).css('top', '-' + current_pos + 'px');
 		$(options.button_slider).css('top', (parseInt(scroll_size/overflow_size*current_pos) + options.slider_top) + 'px');
 	};
